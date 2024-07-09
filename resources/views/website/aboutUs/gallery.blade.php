9c7fafff91f8e7b30a71d671eef554405dec5a4d
XAPHNE <suvnkr.xaphene@gmail.com>
2024-07-09 17:08:43 +0530

    Created gallery view

---
+++ resources/views/website/aboutUs/gallery.blade.php
@@ -0,0 +1,252 @@
+@extends('layouts.guest')
+
+@section('content')
+
+    <section class="pt-3 pb-2">
+        <div class="container">
+            <nav aria-label="breadcrumb">
+                <ol class="breadcrumb bread-text">
+                    <li class="breadcrumb-item"><a href="#" class="bread-text"><i class="fas fa-home" style="color:#3ca369;" aria-hidden="true"></i> Home /</a></li>
+                    <li class="breadcrumb-item"><a href="#" class="bread-text">About Us /</a></li>
+                    <li class="breadcrumb-item active bread-text" aria-current="page">GALLERY</li>
+                </ol>
+            </nav>
+        </div>
+    </section>
+    <section class="pt-0">
+        <div class="container">
+            <div class="row">
+                <h4 class="line-vertical">Power Stations</h4>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="galleryitem.php">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb (1).jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumbnail.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <h4 class="line-vertical">Ministers Visit</h4>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb (1).jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumbnail.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <h4 class="line-vertical">Social Responsibility & Allied Activities</h4>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb (1).jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumbnail.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <h4 class="line-vertical">Industrial Meets, Seminars & Workshops</h4>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb (1).jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumbnail.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <h4 class="line-vertical">Other Events</h4>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumb (1).jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="mt-1 mb-3">
+                        <a href="#">
+                            <div class="text-center border">
+                                <img src="{{ asset('website-assets/images/gallery/powerstation/thumbnail.jpg') }}" alt="gallery" width="100%">
+                                <div class="p-2">
+                                    <p class="mb-0 fw-bold" style="font-size: 14px;">
+                                    Namrup Thermal Power Plant
+                                    </p>
+                            </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+@endsection
