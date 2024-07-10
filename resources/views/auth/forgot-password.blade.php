538902136a95b7eb8a7e84e0a501bd732ecfa1cb
XAPHNE <suvnkr.xaphene@gmail.com>
2024-07-10 16:09:38 +0530

    Updated forgot password blade view

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,38 @@
 <x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
 
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
+    <div class="card">
+        <div class="card-body login-card-body">
+            <p class="login-box-msg">You forgot your password? Here you can easily retrieve a new password.</p>
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+            <form method="POST" action="{{ route('password.email') }}">
+                @csrf
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                <!-- Email Address -->
+                <div class="input-group mb-3">
+                    <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="Email" required autofocus>
+                    <div class="input-group-append">
+                        <div class="input-group-text">
+                            <span class="fas fa-envelope"></span>
+                        </div>
+                    </div>
+                    <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                </div>
+
+                <div class="row">
+                    <div class="col-12">
+                        <button type="submit" class="btn btn-primary btn-block">Request new password</button>
+                    </div>
+                <!-- /.col -->
+                </div>
+            </form>
+            <p class="mt-3 mb-1">
+                <a href="{{ url('login') }}">Login</a>
+            </p>
+            <p class="mb-0">
+                <a href="{{ url('register') }}" class="text-center">Register a new membership</a>
+            </p>
         </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
+    </div>
 </x-guest-layout>
