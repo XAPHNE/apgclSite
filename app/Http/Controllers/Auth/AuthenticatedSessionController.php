18fadf85cf70428410b86f90d28f46ad6e33f446
XAPHNE <suvnkr.xaphene@gmail.com>
2024-07-10 15:51:28 +0530

    Updated to import notification class

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Notifications\SendTwoFactorCode;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -28,6 +29,10 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+
+        $request->user()->generateTwoFactorCode();
+
+        $request->user()->notify(new SendTwoFactorCode());
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
