d892143065a0fe0e6ddbc18c44c4d4e66805da4f
XAPHNE <suvnkr.xaphene@gmail.com>
2024-07-10 15:19:36 +0530

    Updated users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -18,8 +20,19 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->string('two_factor_code')->nullable();
+            $table->dateTime('two_factor_expires_at')->nullable();
             $table->timestamps();
         });
+
+        //  Insert admin user
+        DB::table('users')->insert([
+            'name' => 'Admin',
+            'email' => 'admin@apgcl.org',
+            'password' => Hash::make('secret'),
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 
     /**
