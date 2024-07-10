ce1a26169c3d91f1d1518727cea10b38ac77e072
XAPHNE <suvnkr.xaphene@gmail.com>
2024-07-10 13:16:54 +0530

    Added translations for view_more

--- lang/en/homePage.php
+++ lang/en/homePage.php
@@ -2,5 +2,6 @@
 
 return[
     'news_and_events' => 'News and Events',
-    'partners_who_support_us' => 'Partners who support us'
+    'partners_who_support_us' => 'Partners who support us',
+    'view_more' => 'View More',
 ];
