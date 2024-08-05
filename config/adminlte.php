637cf5f702158802c9b368fac4c44f431811284a
XAPHNE <suvnkr.xaphene@gmail.com>
2024-08-05 11:27:48 +0530

    Commented notifications

--- config/adminlte.php
+++ config/adminlte.php
@@ -304,22 +304,22 @@ return [
             'type' => 'fullscreen-widget',
             'topnav_right' => true,
         ],
-        [
-            'type' => 'navbar-notification',
-            'id' => 'my-notification',                // An ID attribute (required).
-            'icon' => 'fas fa-bell',                  // A font awesome icon (required).
-            'icon_color' => 'warning',                // The initial icon color (optional).
-            'label' => 0,                             // The initial label for the badge (optional).
-            'label_color' => 'danger',                // The initial badge color (optional).
-            'url' => 'notifications/show',            // The url to access all notifications/elements (required).
-            'topnav_right' => true,                   // Or "topnav => true" to place on the left (required).
-            'dropdown_mode' => true,                  // Enables the dropdown mode (optional).
-            'dropdown_flabel' => 'All notifications', // The label for the dropdown footer link (optional).
-            'update_cfg' => [
-                'url' => 'notifications/get',         // The url to periodically fetch new data (optional).
-                'period' => 30,                       // The update period for get new data (in seconds, optional).
-            ],
-        ],
+        // [
+        //     'type' => 'navbar-notification',
+        //     'id' => 'my-notification',                // An ID attribute (required).
+        //     'icon' => 'fas fa-bell',                  // A font awesome icon (required).
+        //     'icon_color' => 'warning',                // The initial icon color (optional).
+        //     'label' => 0,                             // The initial label for the badge (optional).
+        //     'label_color' => 'danger',                // The initial badge color (optional).
+        //     'url' => 'notifications/show',            // The url to access all notifications/elements (required).
+        //     'topnav_right' => true,                   // Or "topnav => true" to place on the left (required).
+        //     'dropdown_mode' => true,                  // Enables the dropdown mode (optional).
+        //     'dropdown_flabel' => 'All notifications', // The label for the dropdown footer link (optional).
+        //     'update_cfg' => [
+        //         'url' => 'notifications/get',         // The url to periodically fetch new data (optional).
+        //         'period' => 30,                       // The update period for get new data (in seconds, optional).
+        //     ],
+        // ],
 
         // Sidebar items:
         [
@@ -352,7 +352,7 @@ return [
         [
             'text' => 'About Us',
             'url' => '#',
-            'icon' => 'fas fa-fw fa-bell',
+            'icon' => 'fas fa-fw fa-id-badge',
             'icon_color' => 'green',
             'submenu' => [
                 [
@@ -372,7 +372,7 @@ return [
         [
             'text' => 'Career',
             'url' => '#',
-            'icon' => 'fas fa-fw fa-id-badge',
+            'icon' => 'fas fa-fw fa-id-card-alt',
             'icon_color' => 'green',
             'submenu' => [
                 [
@@ -478,7 +478,7 @@ return [
             'submenu' => [
                 [
                     'text' => 'Admins',
-                    'url' => '#',
+                    'url' => 'addUser',
                 ],
                 [
                     'text' => 'Deleted',
@@ -490,7 +490,7 @@ return [
                 ],
                 [
                     'text' => 'Department',
-                    'url' => '#',
+                    'url' => 'department',
                 ],
                 [
                     'text' => 'Gallery Management',
