c70952af78724b6e403c9af2567cbcb438f7826c
XAPHNE <suvnkr.xaphene@gmail.com>
2024-08-13 16:56:08 +0530

    Updated Links

--- config/adminlte.php
+++ config/adminlte.php
@@ -466,7 +466,7 @@ return [
         ],
         [
             'text' => 'CSR',
-            'url' => '#',
+            'url' => 'admin/corporate-social-responsibility',
             'icon' => 'fas fa-fw fa-hands-helping',
             'icon_color' => 'green',
         ],
