bbf3cc93c9520f108f8ab5f962283c36c9a2d80d
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-29 19:46:35 +0530

    Updated fillable property with name, visibility, news_n_events and new_badge

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Report extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
-    public $fillable = ['description', 'downloadLink'];
+    public $fillable = ['name', 'description','downloadLink', 'visibility', 'news_n_events', 'new_badge'];
 }
