f2d7651a847e007add162a7bc6da7a3acf904c12
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-29 18:32:46 +0530

    Fixed wrong call to translation

--- resources/views/website/documents/standardForms.blade.php
+++ resources/views/website/documents/standardForms.blade.php
@@ -6,7 +6,7 @@
         <nav aria-label="breadcrumb">
             <ol class="breadcrumb bread-text">
                 <li class="breadcrumb-item"><a href="{{ url('/') }}" class="bread-text"><i class="fas fa-home" style="color:#3ca369;" aria-hidden="true"></i> @lang('navigationMenu.home') /</a></li>
-                <li class="breadcrumb-item"><a href="#" class="bread-text">@lang('navigationMenu.certificates') </a></li>
+                <li class="breadcrumb-item"><a href="#" class="bread-text">@lang('navigationMenu.standard_forms') </a></li>
             </ol>
         </nav>
     </div>
@@ -14,13 +14,13 @@
 <section class="pt-0">
     <div class="container">
         <div class="row">
-            <h4 class="line-vertical">@lang('navigationMenu.certificates')</h4>
+            <h4 class="line-vertical">@lang('navigationMenu.standard_forms')</h4>
             <div class="table-responsive">
                 <table id="table" class=" table-bordered table table-striped" style="width:100%">
                     <thead>
                         <tr class="bg-primary">
-                            <th>@lang('table.serial_num')</th>
-                            <th>@lang('table.name')</th>
+                            <th width="10%">@lang('table.serial_num')</th>
+                            <th width="70%">@lang('table.name')</th>
                             <th class="nosort">@lang('table.download')</th>
                         </tr>
                     </thead>
@@ -32,7 +32,7 @@
                             @if ($standardForm->visibility)
                                 <tr>
                                     <td class="text-center">{{ $serial++ }}</td>
-                                    <td>{{ $standardForm->name }}</td>
+                                    <td class="text-start">{{ $standardForm->name }}</td>
                                     <td>
                                         <a href="{{ url($standardForm->downloadLink) }}" target="_blank">
                                             <i class="fas fa-file-download" aria-hidden="true"></i>
