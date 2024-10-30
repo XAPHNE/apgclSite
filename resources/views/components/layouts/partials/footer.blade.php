9d124c50f757cf7e364f3fa056a47c5860858102
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-30 12:55:12 +0530

    Removed text-decorations from links

--- resources/views/components/layouts/partials/footer.blade.php
+++ resources/views/components/layouts/partials/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="main-footer">
-    <strong>Copyright &copy; {{ date('Y') }} - <a href="https://apgcl.org">{{ config('app.name') }}</a>.</strong>
+    <strong>Copyright &copy; {{ date('Y') }} - <a class="text-decoration-none" href="https://apgcl.org">{{ config('app.name') }}</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.0
