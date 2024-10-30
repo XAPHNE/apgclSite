df010e4291ec6f47ca5277c8db644fb489af3901
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-30 12:50:52 +0530

    Updated blade view for authenticated users

--- resources/views/admin/documents/report.blade.php
+++ resources/views/admin/documents/report.blade.php
@@ -20,7 +20,7 @@
             <div class="card-header">
                 <h3 class="d-inline">Reports List</h3>
                 <div class="card-tools">
-                    <button type="button" class="btn btn-default" data-toggle="modal" data-target="#addNewModal" id="addButton"><i class="fas fa-plus"></i></button>
+                    <button type="button" class="btn btn-light" data-bs-toggle="modal" data-target="#addNewModal" id="addButton"><i class="fas fa-plus"></i></button>
                 </div>
             </div>
             <div class="card-body">
@@ -97,9 +97,7 @@
         <div class="modal-content">
             <div class="modal-header bg-success">
                 <h5 class="modal-title" id="modalTitle">Add New Report</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <form id="addUpdateForm" method="POST" enctype="multipart/form-data">
                 @csrf
@@ -121,8 +119,8 @@
                     <div class="row">
                         <div class="col">
                             <div class="form-group">
-                                <label for="downloadLink">Upload File:</label>
-                                <input type="file" class="form-control-file" id="downloadLink" name="downloadLink">
+                                <label class="form-label" for="downloadLink">Upload File:</label>
+                                <input type="file" class="form-control" id="downloadLink" name="downloadLink">
                             </div>
                         </div>
                         <div class="col">
@@ -155,7 +153,7 @@
                 </div>
             
                 <div class="modal-footer justify-content-between">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
                     <button type="submit" class="btn btn-success" id="saveButton">Save</button>
                 </div>
             </form>
@@ -170,15 +168,13 @@
         <div class="modal-content">
             <div class="modal-header bg-danger">
                 <h5 class="modal-title">Confirm Deletion</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <p>Are you sure you want to delete this report?</p>
             </div>
             <div class="modal-footer justify-content-between">
-                <button type="button" class="btn btn-secondary" data-dismiss="modal">No, Cancel</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No, Cancel</button>
                 <form id="deleteForm" method="POST">
                     @csrf
                     @method('DELETE')
@@ -221,6 +217,13 @@
             $('#modalTitle').text('Add New Report');
             $('#addUpdateForm').attr('action', '{{ route('reports.store') }}');
             $('#addUpdateForm').attr('method', 'POST');
+            $('#downloadLink').attr('required', true);
+            $('#addUpdateModal .modal-header').removeClass('bg-warning').addClass('bg-success');
+            $('#saveButton').removeClass('btn-warning').addClass('btn-success');
+
+            // Add asterisk to indicate required field
+            $('label[for="downloadLink"]').html('Upload File: <span style="color: red;">*</span>');
+
             $('#saveButton').text('Save');
             $('#addUpdateForm input[name="_method"]').remove();
             $('#name').val('');
@@ -249,6 +252,12 @@
             $('#name').val(name);
             $('#description').val(description);
             $('#downloadLink').val('');
+            $('#downloadLink').removeAttr('required');
+            $('#addUpdateModal .modal-header').removeClass('bg-success').addClass('bg-warning');
+            $('#saveButton').removeClass('btn-success').addClass('btn-warning');
+
+            // Remove asterisk as field is not required
+            $('label[for="downloadLink"]').html('Upload File:');
 
             // Set toggle states for each checkbox
             $('#isVisible').prop('checked', visibility).change();
@@ -291,6 +300,22 @@
                 icon: 'error',
                 title: 'Error!',
                 text: '{{ session('error') }}',
+            });
+        @endif
+
+        // Check if there are validation errors and display them in SweetAlert
+        @if ($errors->any())
+            let errorMessages = `<ul style="text-align: left;">`;
+            @foreach ($errors->all() as $error)
+                errorMessages += `<li>{{ $error }}</li>`;
+            @endforeach
+            errorMessages += `</ul>`;
+
+            Swal.fire({
+                icon: 'error',
+                title: 'Validation Error',
+                html: errorMessages,
+                confirmButtonText: 'OK'
             });
         @endif
     </script>
