69e2dc1701269cd23acb915c7729f93b8463ffdd
XAPHNE <suvnkr.xaphene@gmail.com>
2024-10-30 12:54:16 +0530

    Updated navbar for bootstrap 5

--- resources/views/components/layouts/partials/navbar.blade.php
+++ resources/views/components/layouts/partials/navbar.blade.php
@@ -4,53 +4,22 @@
         <li class="nav-item">
             <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
-        {{-- <li class="nav-item d-none d-sm-inline-block">
-            <a href="{{ url('/') }}" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item d-none d-sm-inline-block">
-            <a href="#" class="nav-link">Contact</a>
-        </li> --}}
     </ul>
 
     <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-        <!-- Navbar Search -->
-        {{-- <li class="nav-item">
-            <a class="nav-link" data-widget="navbar-search" href="#" role="button">
-                <i class="fas fa-search"></i>
-            </a>
-            <div class="navbar-search-block">
-                <form class="form-inline">
-                    <div class="input-group input-group-sm">
-                        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-                        <div class="input-group-append">
-                            <button class="btn btn-navbar" type="submit">
-                                <i class="fas fa-search"></i>
-                            </button>
-                            <button class="btn btn-navbar" type="button" data-widget="navbar-search">
-                                <i class="fas fa-times"></i>
-                            </button>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </li> --}}
-
-        <!-- Messages Dropdown Menu -->
-        <!-- Add your messages dropdown code here -->
-        <!-- Notifications Dropdown Menu -->
-        <!-- Add your notifications dropdown code here -->
+    <ul class="navbar-nav ms-auto"> <!-- Updated 'ml-auto' to 'ms-auto' for Bootstrap 5 -->
         <li class="nav-item">
             <a class="nav-link" data-widget="fullscreen" href="#" role="button">
                 <i class="fas fa-expand-arrows-alt"></i>
             </a>
         </li>
+        
         <!-- Profile Dropdown Menu -->
         <li class="nav-item dropdown">
-            <a class="nav-link" data-toggle="dropdown" href="#">
+            <a class="nav-link" data-bs-toggle="dropdown" href="#"> <!-- Updated 'data-toggle' to 'data-bs-toggle' -->
                 <i class="fas fa-user-circle fa-lg"></i>
             </a>
-            <div class="dropdown-menu dropdown-menu-right" style="width: 250px; padding: 0; background-color: transparent; border: 0; box-shadow: none;">
+            <div class="dropdown-menu dropdown-menu-end p-0" style="width: 250px; background-color: transparent; border: 0; box-shadow: none;">
                 <div class="card card-widget widget-user">
                     <div class="widget-user-header bg-info">
                         <h3 class="widget-user-username">{{ Auth::user()->name }}</h3>
@@ -75,7 +44,7 @@
                         </a>
                         <div class="dropdown-divider"></div>
                         <a href="#" class="dropdown-item text-danger"
-                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                           onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                             <i class="fas fa-sign-out-alt mr-2"></i> Logout
                         </a>
                         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -85,11 +54,5 @@
                 </div>
             </div>
         </li>
-
-        {{-- <li class="nav-item">
-            <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-                <i class="fas fa-th-large"></i>
-            </a>
-        </li> --}}
     </ul>
 </nav>
