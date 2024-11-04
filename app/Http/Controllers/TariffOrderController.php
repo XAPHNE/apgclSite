ba4f0ff2f3c8e5e087e6f48905ae3ff9d3f0bf02
XAPHNE <suvnkr.xaphene@gmail.com>
2024-11-04 10:41:56 +0530

    Updated tariff orders module

--- app/Http/Controllers/TariffOrderController.php
+++ app/Http/Controllers/TariffOrderController.php
@@ -2,56 +2,30 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\TariffOrder;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Validator;
-use Yajra\DataTables\DataTables;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\File;
 
 class TariffOrderController extends Controller
 {
+    public function websiteIndex(Request $request, $lang)
+    {
+        App::setLocale($lang);
+        $tariffOrders = TariffOrder::latest()->get();
+        return view('website.documents.tariff-orders', compact('tariffOrders'));
+    }
     /**
      * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
-        //
-        $data = TariffOrder::get();
-
-
-        if ($request->ajax()) {
-
-            return DataTables::of($data)
-                ->addIndexColumn()
-                ->addColumn('action', function ($data) {
-
-                    $btn = '<button data-id="' . $data->id . '" class="edit btn btn-primary btn-sm editBtn">
-                           <span class="icon-bg"><i class="fas fa-edit"></i></span>
-                           </button>';
-
-                    if (Auth::check() && Auth::user()->admin == '1') {
-
-                        $btn .= ' <button data-id="' . $data->id . '" class="btn btn-danger btn-sm  deleteBtn">
-                           <span class="icon-bg"><i class="fas fa-trash-alt"></i></span>
-                           </button>';
-
-                    }
-
-                    return $btn;
-                })
-                ->rawColumns(['action'])
-                ->make(true);
-        }
-
-        return view('admin.documents.tariffOrders', compact('data'));
+        $tariffOrders = TariffOrder::latest()->get();
+        return view('admin.documents.tariff-order', compact('tariffOrders'));
     }
 
     /**
      * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
      */
     public function create()
     {
@@ -60,132 +34,107 @@ class TariffOrderController extends Controller
 
     /**
      * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
-
-        $validator = Validator::make($request->all(),[
-
-            'description' => 'required',
-            'uploadFile' => 'required'
-
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'downloadLink' => 'required|file',
+            'visibility' => 'nullable|boolean',
+            'news_n_events' => 'nullable|boolean',
+            'new_badge' => 'nullable|boolean',
         ]);
 
-        if(!$validator->passes()){
-            return response()->json(['code'=>0,'error'=>$validator->errors()->all()]);//->toArray()]);
-       }else{
-
-
-        //store data in database
-        $tariffOrder = new TariffOrder;
-        $tariffOrder->description = $request->input('description');
-        if ($request->hasfile('uploadFile')) {
-            $file = $request->file('uploadFile');
-            $fileName = time() . '_' . $request->uploadFile->getClientOriginalName();
-            $fileAddress = public_path('/admin-assets/Document/TariffOrder');
-            $file->move($fileAddress, $fileName);
-            $tariffOrder->downloadLink = '/admin-assets/Document/TariffOrder' . '/' . $fileName;
+        if ($request->hasFile('downloadLink')) {
+            $fileName = time() . '_' . $request->file('downloadLink')->getClientOriginalName();
+            $filePath = 'admin-assets/Documents/Tariffs/Tariff Orders/' . $fileName;
+            $request->file('downloadLink')->move(public_path('admin-assets/Documents/Tariffs/Tariff Orders/'), $fileName);
         }
-        $query = $tariffOrder ->save();
 
-       if($query){
-            return response()->json(['code'=>1,'msg'=>'Data submitted successfully']);
-        }else{
-            return response()->json(['code'=>2,'msg'=>'Something went wrong']);
-        }
-      
-       }
+        TariffOrder::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'downloadLink' => $filePath,
+            'visibility' => $request->boolean('visibility'),
+            'news_n_events' => $request->boolean('news_n_events'),
+            'new_badge' => $request->boolean('new_badge'),
+        ]);
+
+        return redirect()->back()->with('success', 'Standard form added successfully');
     }
+
 
     /**
      * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(string $id)
     {
         //
     }
 
     /**
      * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(string $id)
     {
         //
-        $tariffOrder = TariffOrder::find($id);
-        return response()->json([
-            'status' => 200,
-            'tariffOrder' => $tariffOrder,
-        ]);
     }
 
     /**
      * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, string $id)
     {
-        //
+        $tariffOrder = TariffOrder::findOrFail($id);
 
-        $validator = Validator::make($request->all(),[
-
-            'editDescription' => 'required',
-            // 'edituploadFile' => 'required'
-
+        $request->validate([
+            'name' => 'required|string',
+            'description' => 'required|string',
+            'downloadLink' => 'nullable|file',
+            'visibility' => 'nullable|boolean',
+            'news_n_events' => 'nullable|boolean',
+            'new_badge' => 'nullable|boolean',
         ]);
 
-        if(!$validator->passes()){
-            return response()->json(['code'=>0,'error'=>$validator->errors()->all()]);//->toArray()]);
-       }else{
+        if ($request->hasFile('downloadLink')) {
+            if (File::exists(public_path($tariffOrder->downloadLink))) {
+                File::delete(public_path($tariffOrder->downloadLink));
+            }
 
-
-        //store data in database
-        $tariffOrder = TariffOrder::find($request->tariffOrderID);
-        $tariffOrder->description = $request->input('editDescription');
-        if ($request->hasfile('edituploadFile')) {
-            $file = $request->file('edituploadFile');
-            $fileName = time() . '_' . $request->edituploadFile->getClientOriginalName();
-            $fileAddress = public_path('/Document/TariffOrder');
-            $file->move($fileAddress, $fileName);
-            $tariffOrder->downloadLink = '/Document/TariffOrder' . '/' . $fileName;
-            unlink(public_path($request->input('edituploadFilelink')));
+            $fileName = time() . '_' . $request->file('downloadLink')->getClientOriginalName();
+            $filePath = 'admin-assets/Documents/Tariffs/Tariff Orders/' . $fileName;
+            $request->file('downloadLink')->move(public_path('admin-assets/Documents/Tariffs/Tariff Orders/'), $fileName);
+        } else {
+            $filePath = $tariffOrder->downloadLink;
         }
-        $query = $tariffOrder ->update();
 
-       if($query){
-            return response()->json(['code'=>1,'msg'=>'Data submitted successfully']);
-        }else{
-            return response()->json(['code'=>2,'msg'=>'Something went wrong']);
-        }
-      
-       }
+        $tariffOrder->update([
+            'name' => $request->name,
+            'description' => $request->description,
+            'downloadLink' => $filePath,
+            'visibility' => $request->boolean('visibility'),
+            'news_n_events' => $request->boolean('news_n_events'),
+            'new_badge' => $request->boolean('new_badge'),
+        ]);
 
+        return redirect()->back()->with('success', 'Standard Form updated successfully');
     }
+
 
     /**
      * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(string $id)
     {
-        //
-        $tariffOrder = TariffOrder::find($id);
-        $file = TariffOrder::where('id', $id)->pluck('downloadLink')->first();
-        unlink(public_path($file));
+        $tariffOrder = TariffOrder::findOrFail($id);
+
+        if (File::exists(public_path($tariffOrder->downloadLink))) {
+            File::delete(public_path($tariffOrder->downloadLink));
+        }
+
         $tariffOrder->delete();
+
+        return redirect()->back()->with('success', 'Standard form deleted successfully');
     }
 }
