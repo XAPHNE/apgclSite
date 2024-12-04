876d3b275361316b0e629eadc0008d4c81ec47e6
XAPHNE <suvnkr.xaphene@gmail.com>
2024-12-04 12:58:42 +0530

    Added Eloquient Relation in Financial Year Model

--- app/Models/FinancialYear.php
+++ app/Models/FinancialYear.php
@@ -16,4 +16,19 @@ class FinancialYear extends Model
     {
         return $this->hasMany(Tender::class);
     }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function updater()
+    {
+        return $this->belongsTo(User::class, 'updated_by');
+    }
+
+    public function deleter()
+    {
+        return $this->belongsTo(User::class, 'deleted_by');
+    }
 }
