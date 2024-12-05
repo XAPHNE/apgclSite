b6185bb3da550054546b6ef82e34e9110c70f54c
XAPHNE <suvnkr.xaphene@gmail.com>
2024-12-05 14:05:20 +0530

    Implemented created by, updated by and deleted by

--- app/Http/Controllers/ActController.php
+++ app/Http/Controllers/ActController.php
@@ -52,6 +52,8 @@ class ActController extends Controller
             'visibility' => $request->boolean('visibility'),
             'news_n_events' => $request->boolean('news_n_events'),
             'new_badge' => $request->boolean('new_badge'),
+            'created_by' => auth()->id(),
+            'updated_by' => auth()->id(),
         ]);
 
         return redirect()->back()->with('success', 'Act added successfully');
@@ -109,6 +111,7 @@ class ActController extends Controller
             'visibility' => $request->boolean('visibility'),
             'news_n_events' => $request->boolean('news_n_events'),
             'new_badge' => $request->boolean('new_badge'),
+            'updated_by' => auth()->id(),
         ]);
 
         return redirect()->back()->with('success', 'Act updated successfully');
@@ -125,6 +128,9 @@ class ActController extends Controller
         if (File::exists(public_path($act->downloadLink))) {
             // File::delete(public_path($act->downloadLink));
         }
+
+        $act->deleted_by = auth()->id();
+        $act->save();
 
         $act->delete();
 
