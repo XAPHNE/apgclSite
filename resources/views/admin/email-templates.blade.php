2e2f4e65de51fbbe3589338899cc6c085238df17
XAPHNE <suvnkr.xaphene@gmail.com>
2025-02-03 16:43:17 +0530

    Remaned file

--- resources/views/admin/email-templates.blade.php
+++ resources/views/admin/email-templates.blade.php
@@ -40,13 +40,23 @@
                                     <button class="btn btn-info view-button"
                                         data-subject="{{ $emailTemplate->subject }}"
                                         data-email_body="{{ $emailTemplate->email_body }}"
-                                        data-signature="{{ $emailTemplate->signature }}"><i title="View" class="fas fa-eye"></i>
+                                        data-signature="{{ $emailTemplate->signature }}"
+                                        data-is_birthday="{{ $emailTemplate->is_birthday }}"
+                                        data-is_joining_aniversery="{{ $emailTemplate->is_joining_aniversery }}"
+                                        data-is_retirement="{{ $emailTemplate->is_retirement }}"
+                                        data-is_holiday="{{ $emailTemplate->is_holiday }}"
+                                        data-event_id="{{ $emailTemplate->event_id }}"><i title="View" class="fas fa-eye"></i>
                                     </button>
                                     <button class="btn btn-warning update-button"
                                         data-id="{{ $emailTemplate->id }}"
                                         data-subject="{{ $emailTemplate->subject }}"
                                         data-email_body="{{ $emailTemplate->email_body }}"
-                                        data-signature="{{ $emailTemplate->signature }}"><i title="Update" class="fas fa-edit"></i>
+                                        data-signature="{{ $emailTemplate->signature }}"
+                                        data-is_birthday="{{ $emailTemplate->is_birthday }}"
+                                        data-is_joining_aniversery="{{ $emailTemplate->is_joining_aniversery }}"
+                                        data-is_retirement="{{ $emailTemplate->is_retirement }}"
+                                        data-is_holiday="{{ $emailTemplate->is_holiday }}"
+                                        data-event_id="{{ $emailTemplate->event_id }}"><i title="Update" class="fas fa-edit"></i>
                                     </button>
                                     <button class="btn btn-danger delete-button"
                                             data-id="{{ $emailTemplate->id }}"><i title="Delete" class="fas fa-trash-alt"></i>
@@ -86,6 +96,49 @@
                     <div class="form-group">
                         <label for="signature" class="required">Signature:</label>
                         <textarea class="form-control" id="signature" name="signature" rows="4" required></textarea>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="hidden" name="is_birthday" value="0">
+                                <label for="is_birthday" class="required">Is Birthday:</label>
+                                <input type="checkbox" class="form-control visibility-toggle" id="is_birthday" name="is_birthday" value="1" data-toggle="toggle" data-on="Yes" data-off="No" data-style="ios" data-onstyle="success" data-offstyle="danger">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="hidden" name="is_birthday" value="0">
+                                <label for="is_joining_aniversery" class="required">Is Joining Aniversery:</label>
+                                <input type="checkbox" class="form-control visibility-toggle" id="is_joining_aniversery" name="is_joining_aniversery" value="1" data-toggle="toggle" data-on="Yes" data-off="No" data-style="ios" data-onstyle="success" data-offstyle="danger">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="hidden" name="is_birthday" value="0">
+                                <label for="is_retirement" class="required">Is Retirement:</label>
+                                <input type="checkbox" class="form-control visibility-toggle" id="is_retirement" name="is_retirement" value="1" data-toggle="toggle" data-on="Yes" data-off="No" data-style="ios" data-onstyle="success" data-offstyle="danger">
+                            </div>
+                        </div>
+                        <div class="col">
+                            <div class="form-group">
+                                <input type="hidden" name="is_birthday" value="0">
+                                <label for="is_holiday" class="required">Is Holiday:</label>
+                                <input type="checkbox" class="form-control visibility-toggle" id="is_holiday" name="is_holiday" value="1" data-toggle="toggle" data-on="Yes" data-off="No" data-style="ios" data-onstyle="success" data-offstyle="danger">
+                            </div>
+                        </div>
+                    </div>
+                    <div id="eventDropdownContainer" style="display: none;">
+                        <div class="form-group">
+                            <label for="event_id" class="required">Holiday Event:</label>
+                            <select class="form-control" id="event_id" name="event_id">
+                                <option value="" disabled selected>Select</option>
+                                @foreach ($events as $event)
+                                    <option value="{{ $event->id }}">
+                                        {{ $event->public_holidays }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
                 </div>
             
@@ -149,7 +202,32 @@
                 ],
                 scrollX: true,
             });
-            
+
+            $('.visibility-toggle').on('change', function () {
+                if ($(this).prop('checked')) {
+                    $('.visibility-toggle').not(this).bootstrapToggle('off');
+                }
+                toggleEventDropdown();
+            });
+
+            // Show/hide event dropdown based on "Is Holiday" toggle
+            function toggleEventDropdown() {
+                if ($('#is_holiday').prop('checked')) {
+                    $('#eventDropdownContainer').show(); // Show event dropdown
+                } else {
+                    $('#eventDropdownContainer').hide(); // Hide event dropdown
+                    $('#event_id').val(''); // Reset event selection
+                }
+            }
+
+            // Call the function on toggle change
+            $('#is_holiday').on('change', function () {
+                toggleEventDropdown();
+            });
+
+            // Call on modal open to set initial state
+            toggleEventDropdown();
+
             // Handle Add Button
             $('#addButton').on('click', function () {
                 $('#modalTitle').text('Add New Email Template');
@@ -162,6 +240,10 @@
                 $('#subject').val('').prop('readonly', false);
                 $('#email_body').val('').prop('readonly', false);
                 $('#signature').val('').prop('readonly', false);
+                $('#event_id').val('').prop('disabled', false);
+                $('.visibility-toggle').each(function () {
+                    $(this).bootstrapToggle('off');
+                });
                 $('#saveButton').show();
                 $('#addUpdateModal').modal('show');
             });
@@ -171,10 +253,26 @@
                 var subject = $(this).data('subject');
                 var email_body = $(this).data('email_body');
                 var signature = $(this).data('signature');
+                var is_birthday = $(this).data('is_birthday');
+                var is_joining_aniversery = $(this).data('is_joining_aniversery');
+                var is_retirement = $(this).data('is_retirement');
+                var is_holiday = $(this).data('is_holiday');
+                var event_id = $(this).data('event_id');
                 $('#modalTitle').text('View Email Template');
                 $('#subject').val(subject).prop('readonly', true);
                 $('#email_body').val(email_body).prop('readonly', true);
                 $('#signature').val(signature).prop('readonly', true);
+                $('#is_birthday').bootstrapToggle(is_birthday ? 'on' : 'off').prop('disabled', true);
+                $('#is_joining_aniversery').bootstrapToggle(is_joining_aniversery ? 'on' : 'off').prop('disabled', true);
+                $('#is_retirement').bootstrapToggle(is_retirement ? 'on' : 'off').prop('disabled', true);
+                $('#is_holiday').bootstrapToggle(is_holiday ? 'on' : 'off').prop('disabled', true);
+                if (is_holiday) {
+                    $('#eventDropdownContainer').show();
+                    $('#event_id').val(event_id).prop('disabled', true);
+                } else {
+                    $('#eventDropdownContainer').hide();
+                    $('#event_id').val('').prop('disabled', true);
+                }
                 $('#addUpdateModal .modal-header').removeClass('bg-success bg-warning').addClass('bg-info');
                 $('#saveButton').hide();
                 $('#addUpdateModal').modal('show');
@@ -186,6 +284,11 @@
                 var subject = $(this).data('subject');
                 var email_body = $(this).data('email_body');
                 var signature = $(this).data('signature');
+                var is_birthday = $(this).data('is_birthday') ? true : false;
+                var is_joining_aniversery = $(this).data('is_joining_aniversery') ? true : false;
+                var is_retirement = $(this).data('is_retirement') ? true : false;
+                var is_holiday = $(this).data('is_holiday') ? true : false;
+                var event_id = $(this).data('event_id');
 
                 $('#modalTitle').text('Update Email Template');
                 $('#addUpdateForm').attr('action', '/admin/email-templates/' + id);
@@ -195,6 +298,18 @@
                 $('#subject').val(subject).prop('readonly', false);
                 $('#email_body').val(email_body).prop('readonly', false);
                 $('#signature').val(signature).prop('readonly', false);
+                $('#event_id').prop('disabled', false);
+                $('#is_birthday').bootstrapToggle(is_birthday ? true : false).prop('disabled', false);
+                $('#is_joining_aniversery').bootstrapToggle(is_joining_aniversery  ? true : false).prop('disabled', false);
+                $('#is_retirement').bootstrapToggle(is_retirement  ? true : false).prop('disabled', false);
+                $('#is_holiday').bootstrapToggle(is_holiday ? 'on' : 'off').prop('disabled', false);
+                if (is_holiday) {
+                    $('#eventDropdownContainer').show();
+                    $('#event_id').val(event_id).prop('disabled', false);
+                } else {
+                    $('#eventDropdownContainer').hide();
+                    $('#event_id').val('').prop('disabled', false);
+                }
                 $('#addUpdateModal .modal-header').removeClass('bg-success bg-info').addClass('bg-warning');
                 $('#saveButton').removeClass('btn-success').addClass('btn-warning');
                 $('#addUpdateModal').modal('show');
@@ -211,6 +326,10 @@
             // Reset modal when closed
             $('#addUpdateModal').on('hidden.bs.modal', function () {
                 $('#addUpdateForm')[0].reset();
+                $('.visibility-toggle').each(function() {
+                        $(this).bootstrapToggle('off');
+                    });
+                $('#eventDropdownContainer').hide();
                 $('#saveButton').show();
             });
         });
